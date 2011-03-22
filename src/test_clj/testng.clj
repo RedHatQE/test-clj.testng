06d6b1b79159f4d314b3c0649646bb0efc78da65
Jeff Weiss <jweiss@redhat.com>
2011-03-22 13:44:15 -0400

    added data-driven to add equiv functionality to dataproviders

--- src/test_clj/testng.clj
+++ src/test_clj/testng.clj
@@ -31,15 +31,34 @@
         basemeta (meta v)
         basename (str (:name basemeta))
         newmeta (class-keys-to-symbol newmeta)
-        defns (doall (for [[count item] (map-indexed vector data)]
-                 `(defn ~(with-meta (symbol (str basename  "-" count)) 
-                           (update-in newmeta [(-> Test .getName symbol) :groups]
-                                      (fn [v a] (vec (conj v a))) basename))
-                    [_#]
-                    (~myfn ~@item))))]
+        defns (for [[count item] (map-indexed vector data)]
+                `(defn ~(with-meta (symbol (str basename  "_" count)) 
+                          (update-in newmeta [(-> Test .getName symbol) :groups]
+                                     (fn [v a] (vec (conj v a))) basename))
+                   [_#]
+                   (~myfn ~@item)))]
     `(do ~@defns)))
 
 (comment 
+
+  (defmacro data-driven [myfn newmeta data]
+  (let [v (resolve myfn)
+        basemeta (meta v)
+        basename (str (:name basemeta))
+        newmeta (class-keys-to-symbol newmeta)
+        addfn (fn [v a] (vec (conj v a)))
+        testsym (-> Test .getName symbol) 
+        defns (doall (for [[count item] (map-indexed vector data)]
+                       (let []
+                        `(defn ~(with-meta (symbol (str basename  "_" count)) 
+                                  (-> (update-in newmeta [testsym :groups]
+                                                 addfn  basename)
+                                      (update-in [testsym :dependsOnMethods]
+                                                 (if (> count 0) addfn identity)
+                                                 ())))
+                           [_#]
+                           (~myfn ~@item)))))]
+    `(do ~@defns)))
 
          (defn ^{Test {:group ["dd-group-x" "othergroup"]} :enabled false} mytest [_]
            (apply myfn item)))
