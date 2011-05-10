c63fb5c25bc3946d3c9ff932d73062f6cb535a47
Jeff Weiss <jweiss@redhat.com>
2011-05-10 17:30:53 -0400

    clean up, add docs, add ability to mark individual data rows with testng metadata. Still some weirdness with classes/symbols but its at least working

--- src/test_clj/testng.clj
+++ src/test_clj/testng.clj
@@ -1,4 +1,5 @@
 (ns test-clj.testng
+  (:use [clojure.contrib.map-utils :only [deep-merge-with]])
   (:import [org.testng.annotations AfterClass AfterGroups AfterMethod AfterSuite AfterTest	 
 	    BeforeClass BeforeGroups BeforeMethod BeforeSuite BeforeTest Test]))
 
@@ -17,48 +18,48 @@
 	    (vals m))]
     (merge rest sm)))
 
-(defmacro gen-class-testng []
-  (let [publics (vals (ns-publics *ns*))
-	tests (filter test? publics)
-	methods (map (fn [test]
-		       (let [name (method-name test)]
-			 `[~(with-meta (symbol name) (class-keys-to-symbol (meta test))) [] ~'void])) tests)]
+(defn symbol-keys-to-class [m]
+  (zipmap (for [k (keys m)]
+            (if (symbol? k)
+              (let [r (resolve k)]
+                (if (class? r) r k))
+              k))
+          (vals m)))
+
+
+(defmacro gen-class-testng
+  "Generates an ahead-of-time compiled java class from whatever
+  namespace it's called from.  Any functions in the namespace with
+  metadata containing TestNG annotations will be turned into TestNG
+  test methods."
+  []
+  (let [methods (for [test (filter test? (vals (ns-publics *ns*)))] 
+                  `[~(with-meta (symbol (method-name test))
+                       (class-keys-to-symbol (meta test))) [] ~'void])]
     `(gen-class :prefix "" :name ~(symbol (namespace-munge *ns*)) :methods [~@methods])))
 
 
-(defmacro data-driven [myfn newmeta data]
-  (let [v (resolve myfn)
-        basemeta (meta v)
-        basename (str (:name basemeta))
-        newmeta (class-keys-to-symbol newmeta)
-        defns (for [[count item] (map-indexed vector data)]
+(defmacro data-driven
+  "Generates data driven tests.  One TestNG test method will be
+generated for each item in 'data' (which should be a list of argument
+lists).  Each generated TestNG method will execute fn 'myfn' but with
+a different argument list.  The TestNG annotations for each method
+will come from 'newmeta', a map.  Note TestNG annotation classes must
+be fully qualified.  Finally, each item in 'data' can have its own
+metadata, which will be merged with 'newmeta'. "
+
+ [myfn newmeta data]
+  (let [basename (-> myfn resolve meta :name str) 
+        newmeta (symbol-keys-to-class newmeta)
+        merge-fn (fn [a b] (if (coll? a) (vec (concat a b)) b))
+        defns (for [[count item] (map-indexed vector (eval data))]
                 `(defn ~(with-meta (symbol (str basename  "_" count)) 
-                          (update-in newmeta [(-> Test .getName symbol) :groups]
-                                     (fn [v a] (vec (conj v a))) basename))
+                          (deep-merge-with merge-fn
+                                           (update-in newmeta [Test :groups]
+                                                      (fn [v a] (vec (conj v a))) basename)
+                                           (or (meta item) {})))
                    [_#]
                    (~myfn ~@item)))]
     `(do ~@defns)))
 
-(comment 
 
-  (defmacro data-driven [myfn newmeta data]
-  (let [v (resolve myfn)
-        basemeta (meta v)
-        basename (str (:name basemeta))
-        newmeta (class-keys-to-symbol newmeta)
-        addfn (fn [v a] (vec (conj v a)))
-        testsym (-> Test .getName symbol) 
-        defns (doall (for [[count item] (map-indexed vector data)]
-                       (let []
-                        `(defn ~(with-meta (symbol (str basename  "_" count)) 
-                                  (-> (update-in newmeta [testsym :groups]
-                                                 addfn  basename)
-                                      (update-in [testsym :dependsOnMethods]
-                                                 (if (> count 0) addfn identity)
-                                                 ())))
-                           [_#]
-                           (~myfn ~@item)))))]
-    `(do ~@defns)))
-
-         (defn ^{Test {:group ["dd-group-x" "othergroup"]} :enabled false} mytest [_]
-           (apply myfn item)))
